73cb5f6557b2f4ae810aa84e89c4f297f31f936d
pilox <marcelgodovsky@seznamcz>
2018-01-17 13:03:33 +0100

    GoodWith

--- joysticky/src/main.cpp
+++ joysticky/src/main.cpp
@@ -9,7 +9,6 @@ int hozdead= 500 ;
 int verdead= 500 ;
 int distanc= 20 ;
 int data;
-int oldpos=100;
 int val = 0;
 int val2 = 0;
 void setup() {
@@ -17,15 +16,10 @@ void setup() {
     mySwitch.enableTransmit(8);
 }
 void loop() {
-    //Read JoyStick
+   
     val = analogRead(2);
     val2 = analogRead(3);
-   /*   Serial.print(val);
-   Serial.print(",");
-   Serial.println(map(val,0,1023,0,255));
-    Serial.println(val2);
-    delay(200);
-*/
+   
 data=10000;
   //UP 1
 if (val> verdead+distanc){data=data+map(val,530,1023,0,9)*100+1000;}
@@ -35,12 +29,6 @@ if (val< verdead-distanc){data=data+map(val,490,0,0,9)*100+2000;}
 if (val2> hozdead+distanc){data=data+map(val2,530,1024,0,8)+10;}
 //LEFT 4
 if (val2< hozdead-distanc){data=data+map(val2,490,0,0,8)+20;}
-//DEAD ZONE 5
-//if (val>verdead-30 && val< verdead+30 && val2>hozdead-30 && val2< hozdead+30)
-//{
-//data=11;
-//}
-   // Serial.println(data);
-    //delay(50);
+
 mySwitch.send(data, 23);
 }
